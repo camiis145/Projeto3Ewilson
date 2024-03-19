e4bba2ced9467e0372bb670aa794e1397b802ad9
camiis145 <105920509+camiis145@users.noreply.github.com>
2024-03-19 07:58:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao")
-for(let i = 0 < botoes.length; j++){
+for(let i = 0; i < botoes.length; i++){
     botoes[i].onclick= fuction(){
         for(let j = 0 j< botoes.length; j++){
             botoes[j].classList.remove("ativo")
