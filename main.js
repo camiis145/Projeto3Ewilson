11909cbe8568ad96da8e5eff31ab7a524e460687
camiis145 <105920509+camiis145@users.noreply.github.com>
2024-04-30 08:34:42 -0300

    v

--- main.js
+++ main.js
@@ -38,9 +38,9 @@ segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
-return dias + "dias";
-+ horas + "horas";
-+ minutos + "minutos";
+return dias + "dias"
++ horas + "horas"
++ minutos + "minutos"
 + segundos + "segundos";
 
 }
